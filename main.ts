6e4627073d3a0906c22a110037ed0e3903749b37
Jakub Turski <yacoob@ftml.net>
2025-04-04 18:34:14 +0100

    feat: add more calendar plugins

--- main.ts
+++ main.ts
@@ -1,12 +1,23 @@
 import { Plugin } from "obsidian";
+
 import Calendar from "@event-calendar/core";
+import DayGrid from "@event-calendar/day-grid";
 import TimeGrid from "@event-calendar/time-grid";
+import List from "@event-calendar/list";
 
 export default class EventCalendarPlugin extends Plugin {
-  public Calendar = Calendar;
-  public TimeGrid = TimeGrid;
+  public Calendar: any = null;
+  public AllPlugins: any = null;
 
   async onload() {
-    console.log("magical Event Calendar powers activate!");
+    this.Calendar = Calendar;
+    this.AllPlugins = [DayGrid, TimeGrid, List];
+    console.log("...magical Event Calendar powers activate!");
+  }
+
+  async onunload() {
+    this.Calendar = null;
+    this.AllPlugins = null;
+    console.log("Event Calendar plugin unloaded.");
   }
 }
