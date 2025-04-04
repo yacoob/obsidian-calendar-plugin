992dd633abb74e07f98b0c381284e2de568ba788
Jakub Turski <yacoob@ftml.net>
2025-04-04 16:46:19 +0100

    cleanup: retab main.ts

--- main.ts
+++ main.ts
@@ -3,10 +3,10 @@ import Calendar from "@event-calendar/core";
 import TimeGrid from "@event-calendar/time-grid";
 
 export default class EventCalendarPlugin extends Plugin {
-	public Calendar = Calendar;
-	public TimeGrid = TimeGrid;
+  public Calendar = Calendar;
+  public TimeGrid = TimeGrid;
 
-	async onload() {
-		console.log("magical Event Calendar powers activate!");
-	}
+  async onload() {
+    console.log("magical Event Calendar powers activate!");
+  }
 }
