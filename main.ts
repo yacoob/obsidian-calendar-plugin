6191a177cf1ad7b00c94eff18a62e37e3e306c57
Jakub Turski <yacoob@ftml.net>
2025-04-25 23:16:24 +0100

    feat: allow for event selection via dataview query

--- main.ts
+++ main.ts
@@ -1,6 +1,7 @@
 import { Plugin } from "obsidian";
-
+import { getAPI } from "obsidian-dataview";
 import { Calendar } from '@fullcalendar/core';
+
 import CalendarWrapper from 'wrapper';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import timeGridPlugin from '@fullcalendar/timegrid';
@@ -8,12 +9,12 @@ import listPlugin from '@fullcalendar/list';
 
 const allPlugins = [dayGridPlugin, timeGridPlugin, listPlugin];
 
-function createCalendar(calendarEl: HTMLElement, url: string): void {
+function createCalendar(calendarEl: HTMLElement, events: string | Array<object>): void {
   const c = new Calendar(calendarEl, {
     plugins: allPlugins,
     initialView: 'dayGridMonth',
     height: 900,
-    events: url,
+    events: events,
     headerToolbar: { left: 'prev,next today', center: 'title', right: 'dayGridMonth,timeGridWeek,listWeek' },
   });
   c.render();
@@ -23,6 +24,11 @@ function dummyCalendar(calendarEl: HTMLElement): void {
   createCalendar(calendarEl, 'https://fullcalendar.io/api/demo-feeds/events.json');
 }
 
+function actualCalendar(calendarEl: HTMLElement, events: Array<object>): void {
+  // const dv = getAPI();
+  createCalendar(calendarEl, events);
+}
+
 export default class CalendarWrapperPlugin extends Plugin {
   public Calendar = Calendar;
   public AllPlugins = allPlugins;
@@ -30,6 +36,7 @@ export default class CalendarWrapperPlugin extends Plugin {
 
   public createCalendar = createCalendar;
   public dummyCalendar = dummyCalendar;
+  public actualCalendar = actualCalendar;
 
   async onload() {
     console.log("...magical Calendar Wrapper powers activate!");
